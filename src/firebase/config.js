7a66cfa7639bed624974c7b6ad89e25e8029a923
gI7DUmmY <cjmch@inboxen.org>
2021-02-07 11:10:13 +0100

    :heavy_plus_sign: firebase/firestore + config

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1yVeonItvaUnyvq4NoXmeR4LkPCBo7qg",
+  authDomain: "ffft-db.firebaseapp.com",
+  projectId: "ffft-db",
+  storageBucket: "ffft-db.appspot.com",
+  messagingSenderId: "744815547637",
+  appId: "1:744815547637:web:1fca2ee601689fc7b121bc"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init firestore service
+const db = firebase.firestore()
+
+export { db }
