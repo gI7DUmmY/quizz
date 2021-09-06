04dbb5cbc21963d09afdf3b3fe6df8a97016386d
Cédric Rollo <cedric.rollo@iut-tlse3.fr>
2021-09-06 17:11:55 +0200

    :lipstick: css page 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="container center-align">
-    <h3 class="">La page que vous avez demandée est introuvable</h3>
-    <router-link :to="{ name: 'Home' }" class="btn-small">
-      <i class="material-icons left">home</i>Retour à l'accueil
-    </router-link>
-    <img src="@/assets/notFound.svg" alt="404_svg" class="responsive-img" />
+    <div class="row">
+      <div class="col s12"><h3 class="">La page que vous avez demandée est introuvable</h3></div>
+      <div class="col s12">
+        <router-link :to="{ name: 'Home' }" class="btn-small">
+          <i class="material-icons left">home</i>Retour à l'accueil
+        </router-link>
+      </div>
+      <div class="col s12"><img src="@/assets/notFound.svg" alt="404_svg" class="responsive-img" /></div>
+    </div>
   </div>
 </template>
 
